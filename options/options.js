8024ceb197fdd3f600a022225a6ac6edbf7eeb7e
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 18:13:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-credit-dev-133360",
+  url: "https://autumn-credit-dev-133360.botics.co",
+  api: "https://autumn-credit-dev-133360.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
